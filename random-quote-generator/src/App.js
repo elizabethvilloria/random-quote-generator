17480c3f4b3cc64b2acbdaed502f92108aded950
elizabethvilloria <lizvilloria@gmail.com>
2024-08-19 10:44:20 +0200

    Initial setup: Cleaned up files and added basic structure

---
+++ random-quote-generator/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Random Quote Generator</h1>
+      <p>Click the button to get a random quote.</p>
+      <button>Get Quote</button>
+    </div>
+  );
+}
+
+export default App;
